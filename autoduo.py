2e9ca50576a56431a80362dae5af8b96ba0eb33b
MirroredNova <novawiltzius@gmail.com>
2020-01-22 00:26:51 -0600

    minor changes

--- autoduo.py
+++ autoduo.py
@@ -6,6 +6,16 @@ from selenium.webdriver.common.keys import Keys
 
 import duoconfig
 
+#there are different types of challenges
+#div data-test="challenge-select" is pick one of the three pictures
+#div data-test="challenge-judge" is a pick one of the three options for the translation
+#div data-test="challenge-listenTap" is a multiple choice listening question
+#div data-test="challenge-listen" is a written listening question
+#div data-test="challenge-tapComplete" is a fill in the blank question
+#div data-test="challenge-translate" is a translate the sentence problem
+#div data-test="challenge-form" is a fill in the blank but more conjugation based
+#div data-test="challenge-name" is a single word translate
+
 
 def main():
     driver = webdriver.Firefox()
@@ -38,6 +48,13 @@ def start_lesson(driver):
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
             (By.XPATH, "//div[@class='_3ykek']/div[@class='_3bahF _3J-7b']/a[@data-test='global-practice']"))).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//div[@class='_1cw2r']/button[@data-test='player-next']"))).click()
+
+
+#def answer_question():
 
 
 main()
